1f0c3b9266440a9781bf0192627b488c1a62863f
devserver2 <delimce@gmail.com>
2022-01-23 18:14:06 +0000

    Cursonet 2 over php 8.0 adjust and lumen upgrade vers: 8.3.1

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,13 +2,12 @@
 
 namespace App\Exceptions;
 
-use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Illuminate\Http\Response;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -29,32 +28,27 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception $e
+     * @param  \Throwable  $exception
      * @return void
+     *
+     * @throws \Exception
      */
-    public function report(Exception $e)
+    public function report(Throwable $exception)
     {
-        parent::report($e);
+        parent::report($exception);
     }
 
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \Exception $e
-     * @return Response
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Throwable
      */
-    public function render($request, Exception $e)
+    public function render($request, Throwable $exception)
     {
-        if ($e instanceof HttpException) {
-            if ($e->getStatusCode() == 404) {
-                return new Response(
-                    view('errors.404'),
-                    $e->getStatusCode(),
-                    $e->getHeaders()
-                );
-            }
-        }
-        return parent::render($request, $e);
+        return parent::render($request, $exception);
     }
 }
